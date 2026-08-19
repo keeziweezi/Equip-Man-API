1da63780bddfb3f89499926d7daf3bc62d799fc5
kbrozin <27162176@sun.ac.za>
2026-08-19 09:39:41 +0000

    accepting changes

--- app/main.py
+++ app/main.py
@@ -29,7 +29,6 @@ bookings = []
 def root():
     return {"message": "Equipment Management API is running"}
 
-<<<<<<< HEAD
 #Create equipment
 @app.post("/equipment")
 def create_equipment(name: str, category: str, serial_number: str, db: Session = Depends(get_db)):
@@ -38,8 +37,6 @@ def create_equipment(name: str, category: str, serial_number: str, db: Session =
     db.commit()
     db.refresh(equipment)
     return equipment
-=======
->>>>>>> 06fab174fc6b25722a03a0a8cc313224d849f195
 
 @app.get("/equipment")
 def list_equipment():
@@ -61,7 +58,6 @@ def list_equipment(id: int, name: str, category: str):
 def list_equipment():
     return {"equipment": equipment}
 
-<<<<<<< HEAD
 
 @app.get("/equipment/{equipment_id}")
 def get_equipment(equipment_id: int, db: Session = Depends(get_db)):
@@ -87,25 +83,6 @@ def update_equipment(equipment_id: int, name: str, category: str,
 
 
 # ---------- BOOKINGS ----------
-=======
-#Retrieve one equipment
-@app.get("/equipment/{id}")
-def get_equipment(id: int):
-    for item in equipment:
-        if item["id"] ==id:
-            return item
-        return {"message": "Equipment not found"}
-
-#Update equipment
-@app.put("/equipment/{id}")
-def update_equipment(id: int, name: str, category: str):
-    for item in equipment:
-        if item["id"] == id:
-            item["name"] = name
-            item["category"] = category
-            return item
-        return {"message": "Equipment not found"}
->>>>>>> 06fab174fc6b25722a03a0a8cc313224d849f195
 
 #Create booking
 @app.post("/bookings")
@@ -166,8 +143,4 @@ def delete_booking(id: int):
         if booking["id"] == id:
             bookings.remove(booking)
             return {"message": "Booking cancelled"}
-<<<<<<< HEAD
         return{"message": "Booking not found"}"""
-=======
-        return{"message": "Booking not found"}
->>>>>>> 06fab174fc6b25722a03a0a8cc313224d849f195
