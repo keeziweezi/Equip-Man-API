d358c795a73fca7eb9326d95aec764cb259781e8
Keziah <keziah@ubundi.co.za>
2026-08-19 11:36:24 +0200

    merge

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app import models
@@ -8,28 +7,10 @@ models.Base.metadata.create_all(bind = engine)
 
 app = FastAPI()
 
-=======
-from datetime import date
-
-from fastapi import FastAPI, Depends, HTTPException
-from sqlalchemy.orm import Session
-
-from app import models
-from app.database import engine, get_db
-
-models.Base.metadata.create_all(bind=engine)
-
-app = FastAPI()
-
-equipment = []
-bookings = []
-
->>>>>>> 06fab174fc6b25722a03a0a8cc313224d849f195
 @app.get("/")
 def root():
     return {"message": "Equipment Management API is running"}
 
-<<<<<<< HEAD
 #Create equipment
 @app.post("/equipment")
 def create_equipment(name: str, category: str, serial_number: str, db: Session = Depends(get_db)):
@@ -38,8 +19,6 @@ def create_equipment(name: str, category: str, serial_number: str, db: Session =
     db.commit()
     db.refresh(equipment)
     return equipment
-=======
->>>>>>> 06fab174fc6b25722a03a0a8cc313224d849f195
 
 @app.get("/equipment")
 def list_equipment():
@@ -61,33 +40,6 @@ def list_equipment(id: int, name: str, category: str):
 def list_equipment():
     return {"equipment": equipment}
 
-<<<<<<< HEAD
-
-@app.get("/equipment/{equipment_id}")
-def get_equipment(equipment_id: int, db: Session = Depends(get_db)):
-    equipment = db.query(models.Equipment).filter(
-        models.Equipment.id == equipment_id).first()
-    if not equipment:
-        raise HTTPException(status_code=404, detail="Equipment not found")
-    return equipment
-
-
-@app.put("/equipment/{equipment_id}")
-def update_equipment(equipment_id: int, name: str, category: str,
-                     db: Session = Depends(get_db)):
-    equipment = db.query(models.Equipment).filter(
-        models.Equipment.id == equipment_id).first()
-    if not equipment:
-        raise HTTPException(status_code=404, detail="Equipment not found")
-    equipment.name = name
-    equipment.category = category
-    db.commit()
-    db.refresh(equipment)
-    return equipment
-
-
-# ---------- BOOKINGS ----------
-=======
 #Retrieve one equipment
 @app.get("/equipment/{id}")
 def get_equipment(id: int):
@@ -105,7 +57,6 @@ def update_equipment(id: int, name: str, category: str):
             item["category"] = category
             return item
         return {"message": "Equipment not found"}
->>>>>>> 06fab174fc6b25722a03a0a8cc313224d849f195
 
 #Create booking
 @app.post("/bookings")
@@ -166,8 +117,4 @@ def delete_booking(id: int):
         if booking["id"] == id:
             bookings.remove(booking)
             return {"message": "Booking cancelled"}
-<<<<<<< HEAD
-        return{"message": "Booking not found"}"""
-=======
         return{"message": "Booking not found"}
->>>>>>> 06fab174fc6b25722a03a0a8cc313224d849f195
